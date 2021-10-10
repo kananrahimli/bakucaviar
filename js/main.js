6461ca26ab723db5f0891e3339973f01edc70130
kananrahimli <kenanrehimli48@gmail.com>
2021-10-10 19:17:41 +0400

    last-commit

--- js/main.js
+++ js/main.js
@@ -130,18 +130,18 @@ $(window).scroll(function() {
 });
 
 var lastScrollTop = 0;
-$(window).scroll(function(event){
-    var st = $(this).scrollTop();
+// $(window).scroll(function(event){
+//     var st = $(this).scrollTop();
 
-    if (st > lastScrollTop || st === 0){
-        $('.logotype').removeClass("UpImg");
-        $('.logotype').addClass("DownImg");
-    } else {
-        $('.logotype').removeClass("DownImg");
-        $('.logotype').addClass("UpImg");
-    }
-    lastScrollTop = st;
-});
+//     if (st > lastScrollTop || st === 0){
+//         $('.logotype').removeClass("UpImg");
+//         $('.logotype').addClass("DownImg");
+//     } else {
+//         $('.logotype').removeClass("DownImg");
+//         $('.logotype').addClass("UpImg");
+//     }
+//     lastScrollTop = st;
+// });
 
 $(document).ready(function(){
     $('').on('hover',function(){;
